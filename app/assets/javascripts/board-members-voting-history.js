6c967c6239f27a9ffc3524aa176b729fe59e41f6
Elnaz Moshfeghian <elnaz.mosh@gmail.com>
2015-05-26 20:37:53 -0700

    refactor board voting history table; add favicon and apple touch icons

---
+++ app/assets/javascripts/board-members-voting-history.js
@@ -0,0 +1,19 @@
+$('body').ready(function () {
+
+  $('.voting-history').DataTable({
+    aaSorting: [], //disable initial sort
+    aoColumns: [
+      { bVisible: false }, //0: hidden board member name for sorting
+      null, //1: photo
+      { iDataSort: 0 }, //2: board member
+      null, //3: agreed
+      null, //4: disagreed
+      null, //5: did not vote
+      { bSortable: false } //6: voting chart
+    ],
+    info: false,
+    paging: false,
+    searching: false
+  });
+  
+});
