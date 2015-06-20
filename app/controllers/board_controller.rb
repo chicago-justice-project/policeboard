89016f3c7b0ba8ecee1ea860bfa5ed8ffa9033b3
Elnaz Moshfeghian <elnaz.mosh@gmail.com>
2015-06-20 13:08:51 -0700

    only show board vote bar label if more than 10%; add title attributes to bar charts

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -7,6 +7,21 @@ class BoardController < ApplicationController
 
   def show
     @member = BoardMember.find(params[:id])
+
+    @vote_bar_chart = [
+      { :class => "agree", :size => @member.votes_agree_rate },
+      { :class => "disagree", :size => @member.votes_dissent_rate },
+      { :class => "novote", :size => @member.votes_abstain_rate}]
+  
+    if @member.votes_agree_rate > 10
+      @vote_bar_chart[0].merge!(:symbol => "fa-check-circle", :display => "#{@member.votes_agree_rate}%")
+    end
+    if @member.votes_dissent_rate > 10
+      @vote_bar_chart[1].merge!(:symbol => "fa-times-circle", :display => "#{@member.votes_dissent_rate}%")
+    end
+    if @member.votes_abstain_rate > 10
+      @vote_bar_chart[2].merge!(:symbol => "fa-ban", :display => "#{@member.votes_abstain_rate}%")
+    end 
   end
 
   def responsibilities
