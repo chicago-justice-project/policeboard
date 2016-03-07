99c2074ea0c293908b82e67e67467259f2366cf4
Anna Tamayo <amtamayo@gmail.com>
2016-03-07 22:59:33 +0000

    add default user in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([ { :email => "annat@webitects.com", :password => "password", :password_confirmation => "password" } ])
