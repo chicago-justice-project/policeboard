b8cad609e5564e62974eea6201916fd081894659
amarie <amtamayo@gmail.com>
2016-04-14 03:49:54 -0500

    adding rule counts

--- app/assets/javascripts/extranet/case.js
+++ app/assets/javascripts/extranet/case.js
@@ -8,4 +8,21 @@ $('#extranet-case-detail').ready(function () {
     return false;
   });
 
+  
+  
+  $('.add_fields').on('click', function(){
+  	time = new Date().getTime()
+  	regexp = new RegExp($(this).data('id'), 'g')
+  	$(this).before($(this).data('fields').replace(regexp, time))
+  	return false;
+  });
+  
+  
+	function add_fields(link, association, content) {  
+	    var new_id = new Date().getTime();  
+	    var regexp = new RegExp("new_" + association, "g");  
+	    $(link).parent().before(content.replace(regexp, new_id));  
+	}
+	
+	
 });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,16 @@ module ApplicationHelper
   def title(page_title)
     content_for (:title) { page_title }
   end
+  
+  def link_to_add_fields(name, f, association, cssClass, title)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+    #raise fields.inspect
+    
+    link_to(name,'#', class: "add_fields", data: {id: 0, fields: fields.gsub("\n","")}) 
+    #link_to name, "#", :onclick => h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"), :class => cssClass, :title => title 
+    #link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+  end
 end
