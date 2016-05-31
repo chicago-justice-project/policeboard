c598ee1ade5c18755c360e4e5cac18a63b0669fa
Anna Tamayo <amtamayo@gmail.com>
2016-05-31 15:05:07 +0000

    modify tiny mce editor (no menubar and single toolbar)

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,3 +1,11 @@
 class Term < ActiveRecord::Base
   belongs_to :board_member
+
+  def start_at_string
+    start.to_s(:db)
+  end
+
+  def start_at_string=(start_at_string)
+    self.start = Time.parse(start_at_string)
+  end
 end
