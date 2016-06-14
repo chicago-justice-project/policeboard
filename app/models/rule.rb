7bde9af97fcbeab476dc447a838f56e6b407f040
Anna Tamayo <amtamayo@gmail.com>
2016-06-14 21:10:52 +0000

    Truncate rule description in case rule drop down

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,5 +1,6 @@
 class Rule < ActiveRecord::Base
+include ActionView::Helpers::TextHelper
   def item
-     "Rule #{code} #{description}"  
+     "Rule #{code} #{ truncate(description, length: 40, separator: ' ')}"  
   end
 end
