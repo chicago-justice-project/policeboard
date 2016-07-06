8be56304ccfa4981e2d3a5e792718da7a0f0534e
Anna Tamayo <amtamayo@gmail.com>
2016-07-06 16:54:29 +0000

    handle inactive cases in welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @cases = Case.where.not(recommended_outcome_id: nil, decided_outcome_id: nil)
+    @cases = Case.where(is_active: true).where.not(recommended_outcome_id: nil, decided_outcome_id: nil)
     @case_outcomes = @cases.group(:recommended_outcome_id, :decided_outcome_id).count
     @board_members = BoardMember.order(board_position: :asc, last_name: :asc).select{ |bm| bm.active == true }
   end
