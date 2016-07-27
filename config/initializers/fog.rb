31b25fb56259a4c190fb88aebfa3f7dab25f05e9
amarie <amtamayo@gmail.com>
2016-07-26 19:31:15 -0500

    delete fog access key config

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -5,8 +5,6 @@ CarrierWave.configure do |config|
 
     config.fog_credentials = {
        provider:              'AWS',                        # required
-       aws_access_key_id:     'AKIAJR7T4PRN3TPSFXPQ',                        # required
-       aws_secret_access_key: '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',                        # required
        region:                'us-west-2'                # optional, defaults to 'us-east-1'
        #,host:                  's3.example.com',             # optional, defaults to nil
        #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
