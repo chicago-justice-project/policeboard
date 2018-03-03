5ba61de647c5ba9fb006234f1ceca271c3b506cf
Matt Meshulam <matt@meshul.am>
2018-03-03 16:04:52 -0600

    Update CarrierWave config for prod

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -2,26 +2,29 @@
 CarrierWave.configure do |config|
 
   if Rails.env.development? || Rails.env.test?
-    # config.fog_directory  = 'policeboard-staging'
-    config.storage = :file
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
   end
 
+  config.fog_provider = 'fog/aws'
+
+  config.fog_credentials = {
+    provider:              'AWS',
+    region:                ENV["AWS_REGION"],               # optional, defaults to 'us-east-1'
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"] || 'awskey',
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"] || 'awssecret'
+  }
+
+  config.fog_directory = 'policeboard-production'
+
+  config.fog_public     = true    # optional, defaults to true
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+
   if Rails.env.production?
-    config.storage = :fog
-
-    config.fog_provider = 'fog/aws'
-
-    config.fog_credentials = {
-      provider:              'AWS',
-      region:                ENV["AWS_REGION"],               # optional, defaults to 'us-east-1'
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_KEY"]
-    }
-
-    config.fog_directory = 'policeboard-production'
-
-    config.fog_public     = true    # optional, defaults to true
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
   end
 end
 
