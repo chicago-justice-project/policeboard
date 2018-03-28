d8bd600e0143e8da7f5a6c3a2f35548037643bc9
Viduranga Wijesooriya <vpowerrc@gmail.com>
2018-03-27 19:15:00 -0600

    fixed undefined method 'id' for nil:NilClass when votes are not available

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,13 +1,13 @@
 class Vote < ActiveRecord::Base
   def self.AGREE
-    Vote.find_by_name("Agree").id
+    Vote.find_by_name("Agree").try(:id)
   end
 
   def self.DISSENT
-    Vote.find_by_name("Dissent").id
+    Vote.find_by_name("Dissent").try(:id)
   end
 
   def self.ABSTAIN
-    Vote.find_by_name("Abstain").id
+    Vote.find_by_name("Abstain").try(:id)
   end
 end
