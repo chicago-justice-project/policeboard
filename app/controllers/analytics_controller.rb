79188ba0e4b66956304da710d719a752c6c994bd
Viduranga Wijesooriya <vpowerrc@gmail.com>
2018-06-03 11:53:48 +0530

    analytics - added 'Number of cases filed by year' - fixes

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -9,12 +9,14 @@ class AnalyticsController < ApplicationController
     decided_cases = []
 
 
-    decided_outcome_ids = Outcome.where(name: ["Returned to Duty", "Settlement", "Suspension", "Termination"]).map(&:id)
     recommended_outcome_ids = Outcome.where(name: ["Suspension", "Termination"]).map(&:id)
+    decided_outcome_ids = Outcome.where(name: ["Returned to Duty", "Settlement", "Suspension", "Termination"]).map(&:id)
+    rank_ids = Rank.where(name: ["Police Officer", "Detective", "Gang Crimes Specialist", "Lieutenant", "Sargeant"]).map(&:id)
+
 
     @cases_filed_by_year_range.each do |year|
-      cases_per_year = Case.where('extract(year from date_initiated) = ?', year).where(recommended_outcome_id: recommended_outcome_ids)
-      decisions_per_year = Case.where('extract(year from date_decided) = ?', year).where(decided_outcome_id: decided_outcome_ids)
+      cases_per_year = Case.includes(:defendant).where('extract(year from date_initiated) = ?', year).where(recommended_outcome_id: recommended_outcome_ids).where(:defendants => {rank_id: rank_ids})
+      decisions_per_year = Case.includes(:defendant).where('extract(year from date_decided) = ?', year).where(decided_outcome_id: decided_outcome_ids).where(:defendants => {rank_id: rank_ids})
 
       total_cases.push(cases_per_year.count) 
       decided_cases.push(decisions_per_year.count) 
