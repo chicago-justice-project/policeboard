0588fc72947c6c11bf9e8945151f26bfd84d85b1
Nicole Ciemniak <nciemniak@gmail.com>
2019-08-14 16:07:11 -0500

    superintendent interface for admin side

--- app/controllers/extranet/superintendents_controller.rb
+++ app/controllers/extranet/superintendents_controller.rb
@@ -1,7 +1,7 @@
 module Extranet
     class SuperintendentsController < Extranet::ApplicationController
       def index
-        @superintendents = Superintendent.all
+        @superintendents = Superintendent.order(start_of_term: :desc)
       end
 
       def new
@@ -11,15 +11,27 @@ module Extranet
       def create
         @superintendent = Superintendent.new(superintendent_params)
         if @superintendent.save
-            redirect_to extranet_superintendents_path, :notice => "Superintendent successfully added"
-          else
-            flash[:error] = "Oops, there was an error adding a new superintendent."
-            render :action => 'new'
-          end
+          # TO DO : the "notice" section does not look good, and is not noticable. do some css work for this 
+          redirect_to extranet_superintendents_path, :notice => "Superintendent successfully added"
+        else
+          # TO DO : flash[:error] does not work, implement some other error display
+          # flash[:error] = "Oops, there was an error adding a new superintendent."
+          render :action => 'new'
+        end
       end
 
       def edit
         @superintendent = Superintendent.find(params[:id])
+      end
+
+      def update
+        @superintendent = Superintendent.find(params[:id])
+        if @superintendent.update_attributes(superintendent_params)
+          redirect_to extranet_superintendents_path, :notice => "Superintendent successfully added"
+        else
+          # TO DO : implement error display
+          render :action => 'edit'
+        end
       end
 
       private
