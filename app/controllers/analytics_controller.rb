9ea9ccd4f384c04c0821429bd5f82f486a250fa4
Nicole Ciemniak <nciemniak@gmail.com>
2019-10-14 15:00:48 -0500

    update code for better readability

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -7,39 +7,9 @@ class AnalyticsController < ApplicationController
     max_num_years = 0 #used to determine how many years will be displayed for x axis on graph
 
     @superintendents.each_with_index do |su, i|
-      num_cases_termination_recommended = []
-      num_cases_termination_decided = []
-      curr_year_start = su.start_of_term
-      su.end_of_term = Date.today if su.end_of_term.nil? #make sure end of term is not nil
-      
-      #count number of cases for each year of superintendents tenure
-      while curr_year_start < su.end_of_term do
-        if curr_year_start.next_year(1).prev_day.to_date < su.end_of_term
-          curr_year_end = curr_year_start.next_year(1).prev_day.to_date
-        else
-          curr_year_end = su.end_of_term
-        end
-
-        num_cases_termination_recommended
-        .push(Case.count_for_outcome(curr_year_start, curr_year_end, Outcome.get_outcome_id("Termination"), 0))
-
-        num_cases_termination_decided
-        .push(Case.count_for_outcome(curr_year_start, curr_year_end, Outcome.get_outcome_id("Termination"), Outcome.get_outcome_id("Termination")))
-
-        #move forward by one year
-        curr_year_start = curr_year_start.next_year(1).to_date
-      end
-
-      selected = i == 0 ? true : false
-
-      datasets.push({
-        id: su.id,
-        num_cases_termination_recommended: num_cases_termination_recommended,
-        num_cases_termination_decided: num_cases_termination_decided,
-        selected: selected
-      })
-
-      max_num_years = num_cases_termination_decided.count if num_cases_termination_decided.count > max_num_years
+      curr_dataset = get_dataset_for_superintendent(su, i)
+      datasets.push(curr_dataset)
+      max_num_years = curr_dataset[:num_cases_termination_decided].count if curr_dataset[:num_cases_termination_decided].count > max_num_years
     end
 
     labels = *(1..max_num_years)
@@ -47,6 +17,44 @@ class AnalyticsController < ApplicationController
     @terminations_by_superintendent_data = Hash("datasets" => datasets, "labels" => labels)
   end
 
+  def get_curr_year_end(curr_year_start, su)
+    if curr_year_start.next_year(1).prev_day.to_date < su.end_of_term
+      curr_year_start.next_year(1).prev_day.to_date
+    else
+      su.end_of_term
+    end
+  end
+
+  def get_dataset_for_superintendent(su, index)
+    num_cases_termination_recommended = []
+    num_cases_termination_decided = []
+    curr_year_start = su.start_of_term
+    su.end_of_term = Date.today if su.end_of_term.nil? #make sure end of term is not nil
+    
+    #count number of cases for each year of superintendents tenure
+    while curr_year_start < su.end_of_term do
+      curr_year_end = get_curr_year_end(curr_year_start, su)
+
+      num_cases_termination_recommended
+      .push(Case.count_for_outcome(curr_year_start, curr_year_end, Outcome.get_outcome_id("Termination"), 0))
+
+      num_cases_termination_decided
+      .push(Case.count_for_outcome(curr_year_start, curr_year_end, Outcome.get_outcome_id("Termination"), Outcome.get_outcome_id("Termination")))
+
+      #move forward by one year
+      curr_year_start = curr_year_start.next_year(1).to_date
+    end
+
+    selected = index == 0 ? true : false
+
+    dataset = {
+      id: su.id,
+      num_cases_termination_recommended: num_cases_termination_recommended,
+      num_cases_termination_decided: num_cases_termination_decided,
+      selected: selected
+    }
+  end
+  
   def cases_filed_by_year
     start_year = (params[:start_year] || Date.today.year - 4).to_i
     end_year = (params[:end_year] || Date.today.year).to_i
