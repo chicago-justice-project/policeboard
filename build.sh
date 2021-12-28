3c6c22ed705b2721485d0d2b0890b068c6d7131e
sterno <sterno@bigbrother.net>
2021-12-28 14:35:14 -0600

    Checking in changes to get Ruby3 beanstalk working

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+if [ -d deploy ]
+then
+  echo "Removing old deploy directory"
+  rm -rf deploy
+fi
+if [ -f policeboard.zip ]
+then
+  rm policeboard.zip
+fi
 mkdir deploy
-zip deploy/policeboard.zip -r * .ebextensions/* .elasticbeanstalk/*  -x 'tmp/*' -x 'log/*'
+zip deploy/policeboard.zip -r * .ebextensions/* .elasticbeanstalk/*  -x 'tmp/*' -x 'log/*' -x 'node_modules/*'
 cd deploy
 unzip policeboard.zip
 mv policeboard.zip ../
