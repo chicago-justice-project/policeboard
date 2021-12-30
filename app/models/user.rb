9c673f5b01dacd6a3eee0cde83a6447cbf26251d
sterno <sterno@bigbrother.net>
2021-12-30 13:50:02 -0600

    Removing signup feature from site because security

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # Original config:
+  # devise :database_authenticatable, :registerable,
+  # :recoverable, :rememberable, :trackable, :validatable
+
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 end
