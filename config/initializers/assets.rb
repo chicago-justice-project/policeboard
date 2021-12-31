e044d7af90b64c1dbaf43ea7283abba3bd801426
sterno <sterno@bigbrother.net>
2021-12-31 16:11:32 -0600

    Updated asset path in case a nil value shows up

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,7 +16,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # May need this later to filter out some paths but above works for now
 Rails.application.config.assets.precompile << Proc.new do |path|
-  if path =~ /\.(css|js)\z/
+  if path.nil?
+    false
+  elsif path =~ /\.(css|js)\z/
     full_path = Rails.application.assets.resolve(path)
     app_assets_path = Rails.root.join('app', 'assets').to_s
     if full_path.starts_with? app_assets_path
