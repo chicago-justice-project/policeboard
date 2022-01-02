447dbd438dbf3761ac52f5631e22ee2f37544169
sterno <sterno@bigbrother.net>
2022-01-02 17:17:15 -0600

    Fixing odd issue with deploy and the assets.rb file.  Apply a 'rescue' to it

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,17 +16,19 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # May need this later to filter out some paths but above works for now
 Rails.application.config.assets.precompile << Proc.new do |path|
-  if path.nil?
-    false
-  elsif path =~ /\.(css|js)\z/
-    full_path = Rails.application.assets.resolve(path)
-    app_assets_path = Rails.root.join('app', 'assets').to_s
-    if full_path.starts_with? app_assets_path
-      puts "including asset: " + full_path
-      true
-    else
-      #puts "excluding asset: " + full_path
-      false
+   if path =~ /\.(css|js)\z/
+     begin
+      full_path = Rails.application.assets.resolve(path)
+      app_assets_path = Rails.root.join('app', 'assets').to_s
+      if full_path.starts_with? app_assets_path
+        puts "including asset: " + full_path
+        true
+      else
+        #puts "excluding asset: " + full_path
+        false
+      end
+     rescue
+       false
     end
   else
     false
